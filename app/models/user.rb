43e6d96e69558539959f6303f0c91cf40f6dc76d
user <user@example.com>
2016-12-11 03:08:33 +0300

    validationUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   belongs_to :ticket
+  validates :name, presence: true
+  validates :email, presence: true,  uniqueness: true
+  validates_format_of :email, :with => /@/
 end
